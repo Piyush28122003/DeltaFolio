f96157996631531800a5a9e14c505ea991a31203
Piyush Srivastava <official.piyushsrivastava@gmail.com>
2026-02-04 10:05:36 +0000

    Risk feature

---
+++ src/main/java/com/delta/Deltafolio/repository/RiskProfileRepository.java
@@ -0,0 +1,14 @@
+package com.delta.Deltafolio.repository;
+
+import com.delta.Deltafolio.model.RiskProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RiskProfileRepository extends JpaRepository<RiskProfile, Long> {
+    Optional<RiskProfile> findByUserId(Long userId);
+    boolean existsByUserId(Long userId);
+}
+
